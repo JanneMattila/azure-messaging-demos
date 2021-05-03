984a8d98c8ee51a3cbe1bbf1654ba1b3f3f0d2b8
Janne Mattila <janne.mattila@hotmail.fi>
2021-05-03 15:16:50 +0300

    Add Event Hub example

---
+++ examples.ps1
@@ -0,0 +1,29 @@
+$eventHubNamespace = "<your event hub namespace name>"
+$eventHubInstance = "<your event hub instance name>"
+$url = "https://$eventHubNamespace.servicebus.windows.net/$eventHubInstance/messages?api-version=2014-01"
+$url
+
+$accessToken = ConvertTo-SecureString -AsPlainText -String (az account get-access-token --resource https://eventhubs.azure.net --query accessToken -o TSV)
+$body = ConvertTo-Json @{
+    "specversion" = "1.0"
+    "type"        = "ERP.Sales.Order.Created"
+    "source"      = "/mycontext"
+    "subject"     = $null
+    "id"          = "C1234-1234-1234"
+    "time"        = [System.DateTime]::UtcNow
+    "datacontenttype" = "application/json"
+    "data" = @{
+        "appinfoA" = "abc"
+        "appinfoB" = 123
+        "appinfoC" = $true
+    }
+}
+$body
+
+Invoke-RestMethod `
+    -Body $body `
+    -ContentType "application/json" `
+    -Method "POST" `
+    -Authentication Bearer `
+    -Token $accessToken `
+    -Uri $url
