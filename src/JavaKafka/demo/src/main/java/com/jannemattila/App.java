99d001d5c041745020fae7b21cae9a934e101843
Janne Mattila <janne.mattila@hotmail.fi>
2021-05-15 20:55:17 +0300

    Add publisher example

--- src/JavaKafka/demo/src/main/java/com/jannemattila/App.java
+++ src/JavaKafka/demo/src/main/java/com/jannemattila/App.java
@@ -15,9 +15,11 @@ public class App
 {
     public static void main(String[] args) throws Exception
     {
+        var topic = "topic1";
         var properties = "Local.properties";
-        if (args.length == 1) {
-            properties = args[0];
+        if (args.length == 2) {
+            topic = args[0];
+            properties = args[1];
         }   
 
         System.out.println("Kafka messaging demo");
@@ -32,6 +34,12 @@ public class App
         props.setProperty("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         
         var producer = new KafkaProducer<String, String>(props);
+
+        for (int i = 0; i < 100; i++)
+        {
+            producer.send(new ProducerRecord<String, String>(topic, Integer.toString(i), Integer.toString(i)));
+        }
+
         producer.close();
     }
 }
