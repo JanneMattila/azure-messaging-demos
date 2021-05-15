0b120f5f6f3a4fc13b59adef187d018b5360e42c
Janne Mattila <janne.mattila@hotmail.fi>
2021-05-15 20:47:55 +0300

    Add simple example for configuration

--- src/JavaKafka/demo/src/main/java/com/jannemattila/App.java
+++ src/JavaKafka/demo/src/main/java/com/jannemattila/App.java
@@ -16,9 +16,9 @@ public class App
     public static void main(String[] args) throws Exception
     {
         var properties = "Local.properties";
-        if (args.length == 2) {
-            properties = args[1];
-        }
+        if (args.length == 1) {
+            properties = args[0];
+        }   
 
         System.out.println("Kafka messaging demo");
         var configurationFile = Path.of(properties).toRealPath().toString();
@@ -27,6 +27,9 @@ public class App
         var inputStream = new FileInputStream(configurationFile);
         props.load(inputStream);
         inputStream.close();
+
+        props.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.setProperty("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         
         var producer = new KafkaProducer<String, String>(props);
         producer.close();
