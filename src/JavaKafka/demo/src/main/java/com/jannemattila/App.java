759f18512457be5744c43834cc2a459dc42becb9
Janne Mattila <janne.mattila@hotmail.fi>
2021-05-15 21:19:55 +0300

    Add consumer example

--- src/JavaKafka/demo/src/main/java/com/jannemattila/App.java
+++ src/JavaKafka/demo/src/main/java/com/jannemattila/App.java
@@ -2,13 +2,14 @@ package com.jannemattila;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.clients.producer.RecordMetadata;
-import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.Producer;
 
 import java.io.FileInputStream;
 import java.nio.file.Path;
+import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Properties;
 
 public class App 
@@ -30,16 +31,39 @@ public class App
         props.load(inputStream);
         inputStream.close();
 
-        props.setProperty("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-        props.setProperty("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
         
         var producer = new KafkaProducer<String, String>(props);
 
-        for (int i = 0; i < 100; i++)
+        for (var i = 0; i < 100; i++)
         {
+            System.out.println("Send: " + i);
             producer.send(new ProducerRecord<String, String>(topic, Integer.toString(i), Integer.toString(i)));
         }
 
         producer.close();
+
+        props.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group1");
+        props.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        var consumer = new KafkaConsumer<String, String>(props);
+
+        var topics = new ArrayList<String>();
+        topics.add(topic);
+
+        consumer.subscribe(topics);
+        for (var i = 0; i < 10; i++)
+        {
+            var messages = consumer.poll(Duration.ofSeconds(15));
+            for (var message : messages) 
+            {
+                System.out.println("Receive: " + message.key());
+            } 
+        }
+
+        consumer.close();
     }
 }
