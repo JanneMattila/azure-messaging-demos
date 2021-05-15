c6b2fe85e9918bb7d229931c27364cbf85a0409f
Janne Mattila <janne.mattila@hotmail.fi>
2021-05-15 17:01:54 +0300

    Add producer placeholders

--- src/JavaKafka/demo/src/main/java/com/jannemattila/App.java
+++ src/JavaKafka/demo/src/main/java/com/jannemattila/App.java
@@ -1,13 +1,32 @@
 package com.jannemattila;
 
-/**
- * Hello world!
- *
- */
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.Producer;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args) throws Exception
     {
-        System.out.println( "Hello World!" );
+        var properties = "Local.properties";
+        if (args.length == 2) {
+            properties = args[1];
+        }
+
+        System.out.println("Kafka messaging demo (" + properties + ")");
+
+        var props = new Properties();
+        var inputStream = new FileInputStream(properties);
+        props.load(inputStream);
+        inputStream.close();
+        
+        var producer = new KafkaProducer<String, String>(props);
+        producer.close();
     }
 }
